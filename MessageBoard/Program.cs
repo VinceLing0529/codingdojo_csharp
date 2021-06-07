188349774f0a98eb9a4a0e1ae526394a520e6f12
VinceLing0529 <ldkxnb@gmail.com>
2021-06-07 10:55:56 -0700

    first commit

---
+++ MessageBoard/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MessageBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            User user1= new User()
+            {
+                    FirstName ="vince",
+                    LastName="Ling",
+                    Age=32
+            };
+            Console.WriteLine(user1.FirstName);
+        }
+    }
+}
---
+++ MessageBoard/User.cs
@@ -0,0 +1,9 @@
+namespace MessageBoard
+{
+    public class User
+    {
+     public string FirstName { get; set; }   
+     public string LastName { get; set; }   
+     public int Age { get; set; }   
+    }
+}
