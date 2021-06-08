d859e03b5a6be0c29a6769272e3198b23a3311dd
VinceLing0529 <ldkxnb@gmail.com>
2021-06-07 18:18:19 -0700

    collections

---
+++ MessageBoard/fundamental.cs
@@ -0,0 +1,13 @@
+namespace MessageBoard
+{
+    public class fundamental
+    {
+         static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ collections/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = {1,2,3,4,5,6,7,8,9};
+            string[] nameArray= new string[] {"Tim","Martin","Nikki","Sara"};
+            bool[] booArray= new bool[] {true,false,true,false,true,false,true,false,true,false};
+            List<string> icecream = new List<string>();
+            icecream.Add("Choloclate");
+            icecream.Add("Vanellia");
+            icecream.Add("Strawbarry");
+            icecream.Add("Watermelon");
+            icecream.Add("Mint");
+            Console.WriteLine(icecream.Count);
+            Console.WriteLine(icecream[2]);
+            icecream.Remove(icecream[2]);        
+            Console.WriteLine(icecream.Count);
+            Dictionary<string,string> user_flour = new Dictionary<string,string>();
+            for(int i =0;i<4;i++)
+            {
+                Random rand = new Random();
+                user_flour.Add(nameArray[i],icecream[rand.Next(0,3)]);
+
+            }
+            foreach (var entry in user_flour)
+                {
+                    Console.WriteLine(entry.Key + " - " + entry.Value);
+                } 
+        }
+    }
+}
