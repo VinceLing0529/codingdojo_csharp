9a606daa02b70be8086d2a5f5e82511517657e59
VinceLing0529 <ldkxnb@gmail.com>
2021-06-07 21:56:41 -0700

    boxing

---
+++ boxing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> too = new List<object>();
+            too.Add(7);
+            too.Add(28);
+            too.Add(-1);
+            too.Add(true);
+            too.Add("chair");
+            int sum=0;
+            for(int i =0;i<too.Count;i++)
+            {
+                if( too[i] is int )
+                {
+                    Console.WriteLine(too[i]);
+                    sum += (int)too[i];
+                }
+                Console.WriteLine(too[i]);
+
+            }
+            Console.WriteLine(sum);
+        }   
+    }
+}
---
+++ boxing/newclass.cs
@@ -0,0 +1,7 @@
+namespace boxing
+{
+    public class newclass
+    {
+        
+    }
+}
