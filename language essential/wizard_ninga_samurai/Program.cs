3e130ff8e6a058f10c96335ffca78bf7f51e81c4
VinceLing0529 <ldkxnb@gmail.com>
2021-06-08 21:07:25 -0700

    wns

---
+++ language essential/wizard_ninga_samurai/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wizard_ninga_samurai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
