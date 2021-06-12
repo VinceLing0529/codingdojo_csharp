b2be626e9601698048f215b7a3ac5175399b74e7
VinceLing0529 <ldkxnb@gmail.com>
2021-06-11 18:51:53 -0700

    Dojo_survay_wv

---
+++ aspmvc2/Dojo_Survay_wv/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Dojo_Survay_wm.Models;
+
+namespace Dojo_Survay_wm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost("survay/result")]
+        public IActionResult Create(Survey newsur)
+        {
+            if(ModelState.IsValid)
+            {
+                // do somethng!  maybe insert into db?  then we will redirect
+                return RedirectToAction("result",newsur);
+            }
+            else
+            {
+                // Oh no!  We need to return a ViewResponse to preserve the ModelState, and the errors it now contains!
+                return View("Index");
+            }
+        }
+
+        public IActionResult result( Survey newsur)
+        {
+            return View(newsur);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
